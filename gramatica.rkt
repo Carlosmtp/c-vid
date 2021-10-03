5900a482c3dacab9d1ea82b7a6cd94907187c28d
Carlos Tovar <carlos.mauricio.tovar@correounivalle.edu.co>
2021-10-03 18:39:04 -0500

    actualizada bnf

--- gramatica.rkt
+++ gramatica.rkt
@@ -12,107 +12,87 @@
 
 ;; La definición BNF para las expresiones del lenguaje:
 
-;producciones de la léxica
-;<identificador> ::= <letter> | {<letter> | 0,...,9}*
-;<letter>        ::= A...Z | a...z
-;<numero>        ::= [-]{0,...,9}* | [-]{0,...,9}* . {0,...,9}*
-;<octal>         ::= x8 "("{0,...,7}+( )")"
-;<caracter>      ::= (')<letter>(')
-;<cadena>        ::= ("){<caracter>}*(")
-;<bool>          ::= true | false
 
-;<programa>      ::= <globales> <expresion>
-;<globales>      ::= "("{<identificador> = <expresion>}")"*(,)
 
-;producciones de tipo expresión
-;<expresion>     ::= <identificador>
-;                ::= &<identificador>
-;                ::= var "(" {<identificador> = <expresion>}*(,) ")" in <expresion>
+;<programa>      ::= <expresion>
+;<expresion>     ::= "global" "(" {<identificador> "=" <expresion>}*(,)")" <expresion>
+;                ::= <identificador>
+;                ::= "&"<identificador>
+;                ::= "var" "(" {<identificador> = <expresion>}*(,) ")" in <expresion>
 ;                ::= sta "(" {<identificador> = <expresion>}*(,) ")" in <expresion>
-;                ::= rec {<identificador> "(" {<identificador>}*(,) ")" = <expresion>}* in <expresion>
-;                ::= @value
-;                ::= unic "(" {<identificador> = <expresion>}*(,) ")" in <expresion>
-;                ::= <octal>
+;                ::= rec {<identificador> "(" {<identificador>}*(,) ")" "=" <expresion>}* in <expresion>
+;                ::= "@value"
+;                ::= unic "(" {<identificador> = <expresion>}*(,) ")" "in" <expresion>
+;                ::= "(x8" {numero}* ")"
 ;                ::= <numero>
 ;                ::= <caracter>
 ;                ::= <cadena>
 ;                ::= <lista>
-;                ::= <vector>
+;                ::= <vect>
 ;                ::= <registro>
-;                ::= <expr-bool>
-;                ::= sequence "(" {<expresion>}+(;) ")" end
-;                ::= if "(" <expr-bool> ")" then <expresion> "[" else <expresion> "]" end
-;                ::= cond { "[" <expresion> <expresion> "]" }* else <expresion>} end
-;                ::= while "(" <exp-bool> ")" do <expresion> done
-;                ::= for "(" <identificador> = <expresion> ")" (to | downto) <expresion> do <expresion> done
-
-;expresiones adicionales 
-;<expresion>     ::= ":" "(" <expresion> <arit-prim> ")"
-;                ::= "o:" "(" <expresion> <arit-prim-octal> ")"
-;                ::= <cad-prim>
+;                ::= "sequence" "(" {<expresion>}+(;) ")" "end"
+;                ::= "if" "(" <expresion> ")" "then" <expresion> "[" "else" <expresion> "]" "end"
+;                ::= cond { "[" <expresion> <expresion> "]" }* "else" <expresion>} "end"
+;                ::= "while" "(" <expresion> ")" "do" <expresion> "done"
+;                ::= for "(" <identificador> "=" <expresion> <for-prim> <expresion>")" "do" <expresion> "done"
+;                ::= <cadprim>
 ;                ::= <list-prim> "(" <expresion> ")"
-;                ::= <vect-prim> 
-;                ::= <reg-prim> 
-;                ::= define <identificador> "lambda" "(" {<expresion>}* ")" <expresion>       
-;                ::= set <identificador> "=" <expresion>
-;                ::= "(" <expresion> {<expresion>}* ")"
- 
-;primitivas de cadenas
-;<cad-prim>      ::= longitud "(" <expresion> ")"
-;<cad-prim>      ::= concatenar "(" {<expresion>}*(,) ")"
+;                ::= <vect-prim>
+;                ::= <reg-prim>
+;                ::= "define" <identificador> "lambda" "(" {<expresion>}* ")" <expresion>
+;                ::= "set" <identificador> "=" <expresion>
+;                ::= "call(" <expresion> {<expresion>}* ")"
+;                ::= "true"
+;                ::= "false"
+;                ::= "compare" "(" <expresion> <pred-prim> <expresion> ")"
+;                ::= <oper-bin-bool> "(" <expresion> "," "expresion" ")"
+;                ::= "¿" <expr-bool>
+;                ::= "not" "(" <expresion> ")"
+;                ::= ":" "(" <expresion> <arit-prim> ")"
+;                ::= "o:" "(" <expresion> <arit-prim-oct-octal> ")"
+;for-prim        ::= "to"
+;                ::= "downto"
+;cad-prim        ::= "longitud" "(" <expresion> ")"
+;                ::= "concatenar" "(" {<expresion>}*(,) ")"
+;lista           ::= "[" {<expresion>}*(;) "]"
+;                ::= "vacia"
+;                ::= "cons" "(" <expresion> <expresion> ")"
+;                ::= "append" "(" <expresion> <expresion> ")"
+;list-prim       ::= "cabeza"
+;                ::= "cola"
+;expr-bool	 ::= "lista?" "(" <expresion> ")"
+;		 ::= "vacio?" "(" <expresion> ")"
+;		 ::= "vect?" "(" <expresion> ")"
+;		 ::= "registro?" "(" <expresion> ")"
+;vect            ::= "vect" "[" {<expresion>}*(;) "]"
+;vect-prim       ::= "ref-vect" "(" <numero> "de" <expresion> ")"
+;                ::= "set-vect" "(" <expresion> "en" <numero> "de" <expresion> ")"
+;registro        ::= "{" {<identificador> ":" <expresion>}+(,) ")"
+;reg-prim        ::= "ref-registro" "(" <identificador> "de" <expresion> ")"
+;                ::= "set-registro" "(" <expresion> "en" <identificador> "de" <expresion> ")"
+;oper-bin-bool   ::= "and"
+;                ::= "or"
+;                ::="xor"
+;pred-prim       ::= "<"
+;                ::= ">"
+;                ::= "<="
+;                ::= ">="
+;                ::= "=="
+;                ::= "<[" <expresion> "]<"
+;                ::= "<(" <expresion> ")<"
+;arit-prim       ::= "+"
+;                ::= "~"
+;                ::= "*"
+;                ::= "/"
+;                ::= "++"
+;                ::= "--"
+;arit-prim-octal ::= "+"
+;                ::= "-"
+;                ::= "*"
+;                ::= "/"
+;                ::= "++"
+;                ::= "--"
 
-;---------- LISTAS -----------
-;<lista>         ::= "[" {<expresion>}*(;) "]"
-;                ::= vacia
-;                ::= cons "(" <expresion> <expresion> ")"
-;                ::= append "(" <expresion> <expresion> ")"
-
-;primitivas de listas
-;<expr-bool>     ::= lista? "(" <expresion> ")"
-;                ::= vacio? "(" <expresion> ")"
-;<list-prim>     ::= cabeza | cola
-
-;---------- VECTORES -----------
-;<vector>        ::= vector "[" {<expresion>}*(;) "]"
-
-;primitivas de vectores
-;<expr-bool>     ::= vector? "(" <expresion> ")"
-;<vect-prim>     ::= ref-vector "(" <numero> de <expresion> ")"
-;                ::= set-vector "(" <expresion> en <numero> de <expresion> ")"
-
-;---------- REGISTROS -----------
-;<registro>      ::= "{" {<identificador> ":" <expresion>}+(,) "}"
-
-;primitivas de registros
-;<expr-bool>     ::= registro? "(" <expresion> ")"
-;<vect-prim>     ::= ref-registro "(" <identificador> de <expresion> ")"
-;                ::= set-registro "(" <expresion> en <identificador> de <expresion> ")"
-
-;---------- BOOLEANOS -----------
-;expresiones booleanas
-;<expr-bool>     ::= <bool>
-;                ::= compare "(" <expresion> <pred-prim> <expresion> ")"
-;                ::= <oper-bin-bool> "(" <expr-bool> "," <expr-bool> ")"
-;                ::= ¿ <expresion> <expresion>
-;                ::= not "(" <expr-bool> ")"
-
-;operadores booleanos
-;<oper-bin-bool> ::= and | or | xor
-
-;predicado de primitivas
-;<pred-prim>     ::= < | > | <= | >= | == | <>
-
-
-;---------- ARITMÉTICA -----------
-;primitivas aritméticas para decimales
-;<arit-prim>     ::= + | - | * | % | / | ++ | --
-
-;primitivas aritméticas para octales
-;<arit-prim-octal>     ::= + <expresion>
-;                      ::= - <expresion>
-;                      ::= * <expresion>
-;                      ::= ++ <expresion>
-;                      ::= -- <expresion>
  
 ;******************************************************************************************
 
